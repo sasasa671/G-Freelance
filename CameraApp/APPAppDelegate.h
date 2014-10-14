4b11d66c289b202210a0dbd8d5a6b239eb687da8
Malik <Seth@fane.co>
2014-10-13 20:25:33 -0400

    Push App
    Push App

---
+++ CameraApp/APPAppDelegate.h
@@ -0,0 +1,13 @@
+
+
+#import <UIKit/UIKit.h>
+
+@class APPViewController;
+
+@interface APPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) APPViewController *viewController;
+
+@end
